4b258432ae8121f5177c4abd8abe7a273edba6f8
angeldavid56y <apgyei427@gmail.com>
2025-05-09 13:37:37 -0500

    Add files via upload

---
+++ pro 3 mayusculas/script.js
@@ -0,0 +1,5 @@
+function convertir() {
+  let texto = document.getElementById("entrada").value;
+let mayusculas = texto.toUpperCase();
+  document.getElementById("resultado").textContent = mayusculas;
+}
